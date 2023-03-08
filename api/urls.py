e136859994ae45cca62b4b6fa3dc7a114e67fe4e
AboHolo <sevamartynov2003@gmail.com>
2023-03-08 03:06:38 +0300

    Fixed as_view arguments

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from api import views
 
 urlpatterns = [
-    path('logs', views.Logs),
+    path('logs', views.Logs.as_view({'get': 'list'})),
     path('launch', views.launch),
     path('get_result', views.get_result)
 ]
