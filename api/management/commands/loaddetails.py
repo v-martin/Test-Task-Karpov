f4d03d10dab4c5c402e7680272ebdd6928c348ec
AboHolo <sevamartynov2003@gmail.com>
2023-03-09 16:02:13 +0300

    Wrote README.md

--- api/management/commands/loaddetails.py
+++ api/management/commands/loaddetails.py
@@ -10,11 +10,9 @@ from api.models import Sale, Detail
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        counter = 0
         with open('df_sales_detail.csv') as f:
             reader = csv.reader(f)
             for row in reader:
-                counter += 1
                 detail, is_created = Detail.objects.get_or_create(
                     sale_id_id=int(row[1]),
                     good=row[2],
@@ -22,9 +20,7 @@ class Command(BaseCommand):
                     date=datetime.strptime(row[4], '%Y-%m-%d %H%M%S'),
                     user_id=row[5]
                 )
-                if is_created:
-                    logger.debug(f'Detail of order {detail.sale_id} created!')
-                else:
-                    logger.debug(f'Detail of order {detail.sale_id} already exists!')
-                if counter > 1000:
-                    return
+                # if is_created:
+                #     logger.debug(f'Detail of order {detail.sale_id} created!')
+                # else:
+                #     logger.debug(f'Detail of order {detail.sale_id} already exists!')
--- api/management/commands/loadsales.py
+++ api/management/commands/loadsales.py
@@ -10,12 +10,9 @@ from api.models import Sale
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        counter = 0
         with open('df_sales.csv') as f:
             reader = csv.reader(f)
             for row in reader:
-                counter += 1
-                print(row[6])
                 sale, is_created = Sale.objects.get_or_create(
                     sale_id=int(row[1]),
                     date=datetime.strptime(row[2], '%d-%m-%y %H:%M'),
@@ -24,11 +21,10 @@ class Command(BaseCommand):
                     price=int(row[5]),
                     user_id=row[6]
                 )
-                if is_created :
-                    logger.debug(f'Sale {sale.sale_id} created!')
-                else:
-                    logger.debug(f'Sale {sale.sale_id} already exists!')
-                if counter > 1000:
-                    return
+                # if is_created :
+                #     logger.debug(f'Sale {sale.sale_id} created!')
+                # else:
+                #     logger.debug(f'Sale {sale.sale_id} already exists!')
+
 
 
